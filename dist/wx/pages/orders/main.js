641e26d7376a0548df338390ab013458e3dda54f
Qi Chen <1249731680@qq.com>
2020-06-29 20:05:50 +0800

    modify

--- dist/wx/pages/orders/main.js
+++ dist/wx/pages/orders/main.js
@@ -112,7 +112,7 @@ if (false) {(function () {
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
   return _c('div', {
     staticClass: "orders"
-  }, [_vm._v("订单页")])
+  }, [_vm._v("报告")])
 }
 var staticRenderFns = []
 render._withStripped = true
